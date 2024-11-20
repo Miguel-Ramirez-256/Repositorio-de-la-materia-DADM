68c10d123212006d6ad676cfbe531eedf5ab15c5
Miguel-Ramirez-256 <ramirezmiguelan1993@gmail.com>
2024-11-20 18:51:09 +0000

    🚧S03S06 Hooks

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
   import PlanPickerItem from './PlanPickerItem.vue';
-  import { ref } from 'vue';
+  import { onMounted, ref } from 'vue';
   // Creando una referencia reactiva
   // para almacenar el plan seleccionado
   const selectedPlan = ref(null)
@@ -9,6 +9,14 @@
     // Almacenar el plan seleccionado
     selectedPlan.value = name;
   };
+  //Intentando obtener la referencia al elemento .plans
+  console.log("Termina de ejecutar el script setup 🥳");
+
+  // registrando el hook mounted
+  onMounted(() =>{
+    console.log("El componente PlanPicker se ha montado ");
+    console.log(document.querySelector('.plans'));
+  })
 </script>
 
 <template>
